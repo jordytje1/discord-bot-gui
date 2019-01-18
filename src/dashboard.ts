73806758862fbf8be6607b8aabc778c5031232a9
Shane Lippard <s.lippard@protonmail.com>
2019-01-18 06:09:57 -0500

    load bot avatar & member list on login

--- src/dashboard.ts
+++ src/dashboard.ts
@@ -6,6 +6,7 @@ import * as mongoose from 'mongoose';
 import * as Mousetrap from 'mousetrap';
 import { timingSafeEqual } from "crypto";
 import { callbackify } from "util";
+import { stat } from "fs";
 
 export const db = mongoose.connection;
 
@@ -46,16 +47,9 @@ export class Dash {
 
   private ready() {
     console.log('Bot online');
-    // set members
-    let guilds: Array<string> = [];
-    this.client.guilds.forEach(g => {
-      guilds.push(g.id);
-    })
-    guilds.forEach(g => {
-      this.client.guilds.get(g).members.forEach(m => {
-        console.log(m.user.username);
-      })
-    })
+    this.online();
+    this.loadMembers();
+    this.loadImage();
   }
 
   private async handleMessage(message: Message) {
@@ -63,5 +57,35 @@ export class Dash {
     return console.log('Bot observed a message.');
   }
 
+  private online() {
+    var status = (document.getElementsByClassName('status')[0] as HTMLElement);
+    status.innerHTML = 'ONLINE';
+    status.style.border = '2px solid #02d6a8';
+    status.style.color = '#02d6a8';
+  }
+
+  private loadImage() {
+    var logo = (document.getElementsByClassName('bot-img')[0] as HTMLImageElement);
+    logo.src = this.client.user.avatarURL;
+  }
+
+  public async loadMembers() {
+    let members: HTMLElement = (document.getElementById('members'));
+    let guilds: Array<string> = [];
+    this.client.guilds.forEach(g => {
+      guilds.push(g.id);
+    })
+    guilds.forEach(g => {
+      this.client.guilds.get(g).members.forEach(m => {
+        var mem = `${m.user.tag}`;
+        var li = document.createElement("li");
+        li.classList.add('member');
+        var memberItem = mem.padEnd(Math.round(25-mem.length), ' ');
+        li.appendChild(document.createTextNode(`${memberItem}`));
+        members.appendChild(li);
+      })
+    })
+  }
+  
 
 }
